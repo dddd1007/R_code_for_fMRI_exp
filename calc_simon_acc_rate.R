7c290c76a3c352beba1e14e973d8d2a7a777857b
Xiaokai Xia <dddd1007@gmail.com>
2019-03-26 12:57:23 +0800

    移除去除outlier的部分，将删除异常值的部分独立出来

--- calc_simon_acc_rate.R
+++ calc_simon_acc_rate.R
@@ -2,22 +2,27 @@
 # - code is "con" or "inc"
 # - hit is 1 and icorrect is 0
 
-calc_simon_acc_rate <- function(code_vector, acc_vector, rt_vector){
-  acc_vector <- as.numeric(acc_vector)
-  sd <- sd(rt_vector)
-  mean <- mean(rt_vector)
-  upper_sd <- mean + 3*sd
-  lower_sd <- mean - 3*sd
+calc_simon_acc_rate <- function(code_vector, acc_vector){
   
-  mark <- c(which(lower_sd > rt_vector), which(rt_vector > upper_sd))
-  
-  if(length(mark) > 0){
-    code_vector <- code_vector[-mark]
-    acc_vector <- acc_vector[-mark]
-  }
+  acc_vector <- as.character(acc_vector)
+  # sd <- sd(rt_vector)
+  # mean <- mean(rt_vector)
+  # upper_sd <- mean + 3*sd
+  # lower_sd <- mean - 3*sd
+  # 
+  # mark <- c(which(lower_sd > rt_vector), which(rt_vector > upper_sd))
+  # 
+  # if(length(mark) > 0){
+  #   code_vector <- code_vector[-mark]
+  #   acc_vector <- acc_vector[-mark]
+  # }
   
   con_num <- which(code_vector == "con")
   inc_num <- which(code_vector == "inc")
+  
+  acc_vector[which(acc_vector == "hit")] <- 1
+  acc_vector[which(acc_vector == "incorrect" | acc_vector == "miss")] <- 0
+  acc_vector <- as.numeric(as.character(acc_vector))
   
   con_acc_rate <- sum(acc_vector[con_num]) /  length(acc_vector[con_num])
   inc_acc_rate <- sum(acc_vector[inc_num]) /  length(acc_vector[inc_num])
